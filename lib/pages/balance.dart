2957fd9873bc75c62b85011a8a9a897944720606
johnnybeatzxxd <johnnybeatzxxd@gmail.com>
2024-04-17 11:35:48 +0300

    separated the balance page from main file

--- lib/pages/balance.dart
+++ lib/pages/balance.dart
@@ -1,11 +1,78 @@
 import "package:flutter/material.dart";
-
+import "dart:math";
 
 class BalancePage extends StatelessWidget {
   const BalancePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+        body: Stack(
+          children: [
+            CustomPaint(
+              size: Size.infinite,
+              painter: LinePainter(),
+            ),
+            Column(
+              children: [
+                AppBar(
+                  backgroundColor: Colors.transparent,
+                  elevation: 0,
+                  title: const Text('BALANCE', style: TextStyle(fontFamily: "PilatExtended",fontSize: 20)),
+                  centerTitle: true,
+                  leading: IconButton(icon: const Icon(Icons.arrow_back_ios_sharp),onPressed: (){},),
+                  actions: [IconButton(onPressed: (){}, icon: const Icon(Icons.settings))],
+                ),
+                 Container(
+                    padding: const EdgeInsets.fromLTRB(0,72,0,23),
+                    child: const Column(
+                      children: [
+                        Text('\$12,580', style: TextStyle(fontSize: 37, fontWeight: FontWeight.bold,fontFamily: "PilatExtended")),
+                      ],
+                    ),
+                  ),
+                 Padding(padding: const EdgeInsets.fromLTRB(32, 0, 32, 27),
+                  child:Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [Container(
+                      width: 151,
+                      height: 40,
+                      decoration: BoxDecoration(borderRadius: BorderRadius.circular(4),color: Colors.black,),
+                    child: const Center(child: Text("Input",style:TextStyle(fontWeight: FontWeight.bold,fontFamily: "PilatExtended",color: Colors.white70))),
+                    ),Container(
+                      width: 151,
+                      height: 40,
+                      decoration: BoxDecoration(borderRadius: BorderRadius.circular(4),color: Colors.white70,),
+                    child: const Center(child:Text("Output",style:TextStyle(fontWeight: FontWeight.bold,fontFamily: "PilatExtended",color: Colors.black))),
+                    ),],
+                ))
+              ],
+            ),
+          ],
+        ),
+      );
   }
+}
+
+class LinePainter extends CustomPainter {
+  final Random random = Random();
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint()
+      ..color = const Color.fromARGB(31, 0, 0, 0)
+      ..strokeWidth = 0.5;
+
+    for (var i = 0; i < size.width; i += 5 + random.nextInt(5)) {
+      canvas.drawLine(Offset(i.toDouble(), 0), Offset(i.toDouble(), size.height), paint);
+    }
+
+    for (var i = 0; i < size.height; i += 5 + random.nextInt(10)) {
+      canvas.drawLine(Offset(0, i.toDouble()), Offset(size.width, i.toDouble()), paint);
+    }
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) => false;
 }
