65a9524782afa9a8670363cadcc24a96a96dc0b9
johnnybeatzxxd <johnnybeatzxxd@gmail.com>
2024-04-17 11:31:14 +0300

    added multiple floder to make the project structure easy to understand

---
+++ lib/pages/balance.dart
@@ -0,0 +1,11 @@
+import "package:flutter/material.dart";
+
+
+class BalancePage extends StatelessWidget {
+  const BalancePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
