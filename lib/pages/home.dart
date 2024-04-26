32e596c6561c610637b9a17a7ea3d96875562ae2
johnnybeatzxxd <johnnybeatzxxd@gmail.com>
2024-04-26 20:29:32 +0300

    added the containers of the numbers

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -10,23 +10,89 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       body: SingleChildScrollView(
         child: Stack(
-          children: [
-            CustomPaint(
-              size: Size.fromHeight(MediaQuery.of(context).size.height),
-              painter: LinePainter(context),
+                children: [
+                  CustomPaint(
+                    size: Size.fromHeight(MediaQuery.of(context).size.height),
+                    painter: LinePainter(context),
+                  ),
+                  Column(
+                    children: [
+                      AppBar(  
+                        backgroundColor: Colors.transparent,
+                        elevation: 0,
+                        title: Text('BALANCE', style: TextStyle(color: Theme.of(context).colorScheme.onBackground, fontFamily: "PilatExtended",fontSize: 20)),
+                        centerTitle: true,
+                        leading: IconButton(icon:  Icon(Icons.arrow_back_ios_sharp,color: Theme.of(context).colorScheme.onBackground,),onPressed: (){},),
+                        actions: [IconButton(onPressed: (){}, icon:  Icon(Icons.settings,color : Theme.of(context).colorScheme.onBackground))],
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                           Container(
+                            height: 52,
+                            width: 50,
+                            decoration: BoxDecoration(
+                              shape: BoxShape.rectangle,
+                              color: Colors.black54,
+                              borderRadius: BorderRadius.circular(6),
+                              border: Border.all(color: Colors.white54),
+                            ),
+                            child: Center(
+                              child: Text((1).toString(), style: TextStyle(color: Theme.of(context).colorScheme.onBackground, fontFamily: "PilatExtended", fontSize: 20)),
+                            ),
+                          ),
+                          Container(
+                            height: 52,
+                            width: 50,
+                            decoration: BoxDecoration(
+                              shape: BoxShape.rectangle,
+                              color: Colors.black54,
+                              borderRadius: BorderRadius.circular(6),
+                              border: Border.all(color: Colors.white54),
+                            ),
+                            child: Center(
+                              child: Text((1).toString(), style: TextStyle(color: Theme.of(context).colorScheme.onBackground, fontFamily: "PilatExtended", fontSize: 20)),
+                            ),
+                          ),
+                          Container(
+                            height: 52,
+                            width: 50,
+                            decoration: BoxDecoration(
+                              shape: BoxShape.rectangle,
+                              color: Colors.black54,
+                              borderRadius: BorderRadius.circular(6),
+                              border: Border.all(color: Colors.white54),
+                            ),
+                            child: Center(
+                              child: Text((2).toString(), style: TextStyle(color: Theme.of(context).colorScheme.onBackground, fontFamily: "PilatExtended", fontSize: 20)),
+                            ),
+                          ),
+                          Container(
+                            height: 52,
+                            width: 50,
+                            decoration: BoxDecoration(
+                              shape: BoxShape.rectangle,
+                              color: Colors.black54,
+                              borderRadius: BorderRadius.circular(6),
+                              border: Border.all(color: Colors.white54),
+                            ),
+                            child: Center(
+                              child: Text((3).toString(), style: TextStyle(color: Theme.of(context).colorScheme.onBackground, fontFamily: "PilatExtended", fontSize: 20)),
+                            ),
+                          ),
+
+                          
+                        ],)
+                          
+                      
+                    ],
+                  ),
+                ],
+              ),
             ),
-             Column(
-              children: [
-                AppBar(  
-                  backgroundColor: Colors.transparent,
-                  elevation: 0,
-                  title: Text('BALANCE', style: TextStyle(color: Theme.of(context).colorScheme.onBackground, fontFamily: "PilatExtended",fontSize: 20)),
-                  centerTitle: true,
-                  leading: IconButton(icon:  Icon(Icons.arrow_back_ios_sharp,color: Theme.of(context).colorScheme.onBackground,),onPressed: (){},),
-                  actions: [IconButton(onPressed: (){}, icon:  Icon(Icons.settings,color : Theme.of(context).colorScheme.onBackground))],
-                ),
-              ],
-            )]),)
-    ,);
+          
+        
+      
+    );
   }
 }
